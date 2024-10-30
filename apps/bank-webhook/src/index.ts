ed7d5c86720b0699032c2af78319ffe3872b7762
Samarth Rajput <ssamarth224@gmail.com>
2024-10-31 01:02:07 +0530

    working on frontend

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -8,42 +8,60 @@ const app = express();
 // this the endpoint that hdfc bank will hit, whenever my user pays hdfc bank some money and hdfc bank transfer that money over to my bank, and tells me ki bro transfer is done
 // pls put 1000 rs in there wallet balance
 
-// here is the userId for your database, and here is the amount that you need to credit to this perso, this specific person has paid this much amount on netbanking.hdfcbank.com
+// here is the userId for your database, and here is the amount that you need to credit to this person, this specific person has paid this much amount on netbanking.hdfcbank.com
 app.post("/hdfcWebhook", async (req, res) => {
-    const paymentInformation = {
+    const paymentInformation : {
+        token: string,
+        userId: number,
+        amount:  number
+    } = {
         token: req.body.token,
         userId: req.body.user_identifier,
         amount: req.body.amount
     }; 
 
-    // Update balance in db, add txn
-    // update the balance where the userId of the user is this, increase the amount by this much  
-    await prisma.balance.update({
-        where: {
-            userId: paymentInformation.userId
-        },
-        // set the data to be 
-        data: {
-            amount: {
-                increment: paymentInformation.amount
-            }
-        }
-    });
+    // Adding transactions 
+    try {
+        await prisma.$transaction([
+            // Update balance in db, add txn
+            // update the balance where the userId of the user is this, increase the amount by this much  
+            prisma.balance.update({
+                where: {
+                    userId: paymentInformation.userId
+                },
+                // set the data to be 
+                data: {
+                    amount: {
+                        increment: paymentInformation.amount
+                    }
+                }
+            }),
+        
+            // the token is how you identify an onRamptransaction, so whenever you are creating this OnRampTransaction from the ui you will first put this entry on you OnRampTransaction table
+            // you will store the token there, eventually when hdfc bank tells you, send the token along thats how you can identifies the specific OnRampTransaction 
+            prisma.onRampTransaction.update({
+                where: {
+                    token: paymentInformation.token
+                },
+                data: {
+                    status: "Success"
+                }
+            })
 
-    // the token is how you identify an onRamptransaction, so whenever you are creating this OnRampTransaction from the ui you will first put this entry on you OnRampTransaction table
-    // you will store the token there, eventually when hdfc bank tells you, send the token along thats how you can identifies the specific OnRampTransaction 
-    await prisma.onRampTransaction.update({
-        where: {
-            token: paymentInformation.token
-        },
-        data: {
-            status: "Success"
-        }
-    });
+        ]);
+        
+            // If you do this, you tell the hdfc bank server, ki bro status 200, i have been able to process the request, i have captured the request 
+            // this is super imp if we dont do this or if we send a bad status code then hdfc bank will feel ki they were not able to capture this request and they will refund the money to user 
+            res.json({
+                message: "captured"
+            })
+    } catch(e){
+        console.error(e);
+        res.status(411).json({
+            message: "Error while processing webhook"
+        })
+    }
 
-    // If you do this, you tell the hdfc bank server, ki bro status 200, i have been able to process the request, i have captured the request 
-    // this is super imp if we dont do this or if we send a bad status code then hdfc bank will feel ki they were not able to capture this request and they will refund the money to user 
-    res.json({
-        message: "captured"
-    })
 });
+
+app.listen(3003);
--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,15 +1,19 @@
 "use client"
-import { useBalance } from "@repo/store/useBalance";
-import { Appbar } from "@repo/ui/appbar";
-import { signIn, signOut, useSession } from "next-auth/react";
 
-export default function Home() {
-  const session = useSession();
-  const balance = useBalance();
-  return (
-    <div className="bg-pink-500 font-black">
-      <Appbar onSignin={signIn} onSignout={signOut} user={session.data?.user}/>
-      Hello world from user app, your balance is {balance}
+import { getServerSession } from "next-auth"
+import { authOptions } from "../lib/auth"
+import { redirect } from "next/navigation";
+
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+
+  if(session?.user){
+    redirect("/dashboard")
+  }
+  else{
+    return <div>
+      
     </div>
-  );
+  }
+
 }
