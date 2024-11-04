9982289d49d037982a31fa3b08eb0bbae4144cd3
Samarth Rajput <ssamarth224@gmail.com>
2024-11-05 01:42:43 +0530

    started working on merchant app

---
+++ apps/merchant-app/app/(dashboard)/layout.tsx
@@ -0,0 +1,32 @@
+import { Appbar } from "@repo/ui/appbar"
+import React from "react"
+import { SidebarItem } from "../../components/SidebarItem"
+
+export default function Layout({ children }: {
+    children: React.ReactNode
+}): JSX.Element {
+    return  <div className="min-h-screen min-w-screen bg-[#ebe6e6]">
+        {/* <AppbarClient /> */}
+        <div className="flex">
+            <div className="w-72 border-r border-slate-300 min-h-screen mr-4 pt-28">
+                <div>
+                    <SidebarItem title="Home" href={"/dashboard"} icon={<HomeIcon />}/>
+                    <SidebarItem title="Transactions" href={"/transactions"} icon={<TransactionIcon />}/>
+                </div>
+            </div>
+            {children}
+        </div>
+    </div>
+}
+
+function HomeIcon(){
+    return <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+        <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+    </svg>
+}
+
+function TransactionIcon(){
+    return <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+    <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+    </svg>
+}
---
+++ apps/merchant-app/app/(dashboard)/transactions/page.tsx
@@ -0,0 +1,5 @@
+export default function (){
+    return <div>
+        Hello from transactions page in merchant app
+    </div>
+}
