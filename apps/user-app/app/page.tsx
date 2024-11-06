677c6e5aa4b10b5c065f17f66d2f937fce5e584b
Samarth Rajput <ssamarth224@gmail.com>
2024-11-06 14:33:26 +0530

    changes done on front page

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -13,9 +13,27 @@ export default async function Home() {
   }
 
   return ( 
-    <div>
+    <div className="">
       <AppbarClient />
-      Hello world
+      <div className="min-h-screen min-w-screen ">
+            <div className=" bg-[#DEEFFF] pl-10 ml-10 mt-10 max-w-4xl rounded-3xl pb-72 pt-12">
+              <div className="text-6xl text-[#2F3033] pt-6 ">
+                Fast, safe,
+              </div>
+              <div className="text-6xl text-[#2F3033] pt-6">
+                social
+              </div>
+              <div className="text-5xl text-[#2F3033] pt-6">
+                Payments
+              </div>
+              
+              <div className="text-2xl pt-10 text-[#2F3033]">
+                Pay, get paid, grow a business, and more. Join the tens of millions of people on Digital Wallet.
+              </div>
+
+            </div>
+
+      </div>  
     </div>
   )
 } 
