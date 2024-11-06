08f5dd7a704a5b1f686d9878ef496ef80b41ac4a
Samarth Rajput <ssamarth224@gmail.com>
2024-11-06 14:40:09 +0530

    updated frontend

--- apps/user-app/app/(dashboard)/dashboard/page.tsx
+++ apps/user-app/app/(dashboard)/dashboard/page.tsx
@@ -1,6 +1,14 @@
+import { getServerSession } from "next-auth"
+import { authOptions } from "../../lib/auth"
+
 export default function(){
 
-    return <div>
-        Dashboard page 
+    const session = getServerSession(authOptions);
+
+
+    return <div className="w-screen">
+        <div className="text-4xl text-[#6a51a6] pt-8 mb-8 font-bold"> 
+            Nice to see you here 
+        </div>
     </div>
 }
--- apps/user-app/app/(dashboard)/transactions/page.tsx
+++ apps/user-app/app/(dashboard)/transactions/page.tsx
@@ -1,6 +1,8 @@
 export default function(){
 
-    return <div>
-        Transactions page 
+    return <div className="w-screen">
+        <div className="text-4xl text-[#6a51a6] pt-8 mb-8 font-bold"> 
+            Recent Transactions
+        </div>
     </div>
 }
