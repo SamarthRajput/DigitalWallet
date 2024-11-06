4ba14affc484a3dec7ee23991b594b9e3269f0d0
Samarth Rajput <ssamarth224@gmail.com>
2024-11-06 14:45:55 +0530

    minor changes

--- apps/user-app/app/(dashboard)/dashboard/page.tsx
+++ apps/user-app/app/(dashboard)/dashboard/page.tsx
@@ -1,10 +1,5 @@
-import { getServerSession } from "next-auth"
-import { authOptions } from "../../lib/auth"
 
 export default function(){
-
-    const session = getServerSession(authOptions);
-
 
     return <div className="w-screen">
         <div className="text-4xl text-[#6a51a6] pt-8 mb-8 font-bold"> 
